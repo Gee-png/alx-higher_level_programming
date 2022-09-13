560f156553a7a15de08c460e4a0ba4df6041d368
Gee-png <gloryamuah@gmail.com>
2022-09-13 23:41:55 +0000

    empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-# 0-square.py by Amuah Glory
-"""Defines a square """
+
+"""Define a class Square"""
 
 
 class Square:
-    """Does nothing"""
-    pass
+    """Empty class"""
+    def __int__(self):
+        pass
+
+
+if __name__ == "__main__":
+    Square()
